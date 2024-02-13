93dda8e5b7b82ee1198022b69334550520923869
18lis <jack0712li@gmail.com>
2024-02-12 19:30:31 -0500

    two sum reveiew

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -8,5 +8,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        # 用于记录查询过的数字
+        record ={}
+        
+        for i in range(len(nums)):
+            left = target - nums[i]
+            # 查询是否已经被记录过，如果记录过则返回
+            if left in record:
+                return [record[left],i]
+            
+            else:
+                record[nums[i]] = i
+        return [0]
+                            
+        
+        
+        
+        
 # @lc code=end
 
