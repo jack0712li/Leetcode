9694adf4e01e70ce418a26079ebb37cc2dfbe235
18lis <jack0712li@gmail.com>
2024-02-15 17:14:25 -0500

    20 stack only

---
+++ 20. Valid Parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        stack = []
+
+        for char in s:
+            if char == "(":
+                stack.append(')')
+            elif char == "[":
+                stack.append(']')
+            elif char == "{":
+                stack.append('}')
+            elif not stack or stack[-1] != char:
+                return False
+            else:
+                stack.pop()
+
+        return True if not stack else False
