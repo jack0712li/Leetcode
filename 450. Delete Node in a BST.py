886d1ef3df13d68cbf6a9c59f17d35214b73a522
18lis <jack0712li@gmail.com>
2024-02-24 23:02:59 -0500

    450 not simlple need review

---
+++ 450. Delete Node in a BST.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        if root is None:
+            return root
+
+        if root.val == key:
+            if not root.left and not root.right:
+                return None
+            elif not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                cur = root.right
+                while cur.left:
+                    cur = cur.left
+                cur.left = root.left
+                return root.right
+        if root.val > key:
+            root.left = self.deleteNode(root.left,key)
+
+        if root.val < key:
+            root.right = self.deleteNode(root.right,key)
+
+        return root
+
+
+        
