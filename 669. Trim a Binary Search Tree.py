70dd80784b39044a892f6505e49d7e2cb46df6f3
18lis <jack0712li@gmail.com>
2024-02-25 21:55:50 -0500

    669 not simple need more thingking

---
+++ 669. Trim a Binary Search Tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+
+        if not root:
+            return None
+
+        if root.val > high:
+            left = self.trimBST(root.left,low,high)
+            return left
+
+        if root.val < low:
+            right = self.trimBST(root.right,low,high)
+            return right
+
+        root.left=self.trimBST(root.left,low,high)
+        root.right = self.trimBST(root.right,low,high)
+
+        return root
